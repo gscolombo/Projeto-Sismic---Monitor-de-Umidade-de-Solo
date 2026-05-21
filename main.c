a3c2af4f6066e952210366f480d1fd8a2200445f
Gabriel S. Colombo <gscolombo404@gmail.com>
2026-05-21 19:31:56 -0300

    Cálculo de percentual de umidade a partir de sinal analógico

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+typedef enum state { IDLE, WORK } state;
+
+static state mode = IDLE;
+
 void setup() {
   WDTCTL = WDTPW | WDTHOLD; // Para watchdog timer
 
@@ -14,19 +18,36 @@ void setup() {
 
 void main() {
   setup();
+  static char buffer[32];
+  volatile unsigned long humidity, humidity_perc;
+  volatile unsigned int integer, decimal;
 
   for (;;)
-    __low_power_mode_1();
+    switch (mode) {
+    case WORK:
+      humidity = ((long)(4095 - analog_read_value) + 1) * 1000;
+      humidity_perc = humidity >> 12; // Divide por 4096
+      integer = humidity_perc / 10;
+      decimal = humidity_perc % 10;
+
+      sprintf(buffer, "ADC: %d | Umidade: %d.%d%%", analog_read_value, integer, decimal);
+      serialPrintLn(buffer);
+      mode = IDLE;
+    case IDLE:
+      __low_power_mode_1();
+      break;
+    default:
+      break;
+    }
 }
 
 #pragma vector = TIMER0_A1_VECTOR
 __interrupt void checkRead() {
-  static char buffer[32];
 
   switch (TA0IV) {
   case TA0IV_TA0IFG:
-    sprintf(buffer, "ADC: %d", analog_read_value);
-    serialPrintLn(buffer);
+    mode = WORK;
+    __low_power_mode_off_on_exit();
     break;
   default:
     break;
